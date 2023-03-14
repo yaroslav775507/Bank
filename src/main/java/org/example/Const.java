943086bddf5931c5b6c32ace1e412464002169ea
yaroslav775507 <redyar55@gmail.com>
2023-03-14 22:46:13 +0300

    Упрощение

---
+++ src/main/java/org/example/Const.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Const {
+    public static final String USER_TABLE = "users";
+    public static final String USER_ID = "id";
+    public static final String USER_NAME = "name";
+    public static final String USER_MONEY = "money";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_GENDER = "gender";
+
+
+}
---
+++ src/main/java/org/example/DataBase.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class DataBase {
+    public static final String URL ="jdbc:mysql://localhost/YRtest";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD ="root1234";
+
+}
